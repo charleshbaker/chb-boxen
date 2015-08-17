ef696f7714a35aaa8b033a74ed034c74ba00a6a3
Charles Baker <charleshbaker@gmail.com>
2015-08-16 22:01:12 -0400

    Added brewcask to install evernote.

--- modules/people/manifests/charleshbaker.pp
+++ modules/people/manifests/charleshbaker.pp
@@ -2,6 +2,7 @@ class people::charleshbaker {
 
   include zsh   # requires zsh module in Puppetfile
   include chrome   # requires chrome module in Puppetfile
+  include brewcask # requires brewcask module in Puppetfile
 
   package { 'evernote': provider => 'brewcask' }
 
