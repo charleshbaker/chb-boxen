fdcc36b381f47584bccc39941eec8d4dff9b267f
Charles Baker <charleshbaker@gmail.com>
2015-08-16 21:52:40 -0400

    Add evernote.

--- modules/people/manifests/charleshbaker.pp
+++ modules/people/manifests/charleshbaker.pp
@@ -1,6 +1,8 @@
-include "zsh"
 class people::charleshbaker {
+
   include zsh   # requires zsh module in Puppetfile
   include chrome   # requires chrome module in Puppetfile
+
+  package { 'evernote': provider => 'brewcask' }
 
 }
