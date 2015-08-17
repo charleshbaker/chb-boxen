5bc2e1de74f842d48fe51c279505a86924172ea8
Charles Baker <charleshbaker@gmail.com>
2015-08-16 22:22:08 -0400

    Using cask example from https://gist.github.com/joshbeard/afb4f37429ac52d6fa3e

--- modules/people/manifests/charleshbaker.pp
+++ modules/people/manifests/charleshbaker.pp
@@ -4,6 +4,24 @@ class people::charleshbaker {
   include chrome   # requires chrome module in Puppetfile
   include brewcask # requires brewcask module in Puppetfile
 
-  package { 'evernote': provider => 'brewcask' }
+  ## Install these with Cask - http://caskroom.io
+  $cask_packages = [
+    'dropbox',
+    'evernote',
+    'firefox',
+    'flux',
+    'github',
+    'hipchat',
+    'iterm2',
+    'vagrant',
+    'virtualbox',
+    'vlc',
+  ]
+
+  package { $cask_packages:
+    ensure        => present,
+    provider      => 'brewcask',
+    require       => Sudoers[$::boxen_user],
+  }
 
 }
