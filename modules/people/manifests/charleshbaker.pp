8a616d47c793cfc5c495959c7c29aa066e9cb609
Charles Baker <charleshbaker@gmail.com>
2015-08-20 10:31:13 -0400

    Switch to textmate2::beta

--- modules/people/manifests/charleshbaker.pp
+++ modules/people/manifests/charleshbaker.pp
@@ -3,7 +3,7 @@ class people::charleshbaker {
   include atom # requires atom module in Puppetfile
   include brewcask # requires brewcask module in Puppetfile
   include chrome   # requires chrome module in Puppetfile
-  include textmate::textmate2::release
+  include textmate::textmate2::beta
   include zsh   # requires zsh module in Puppetfile
 
   ## Install these with Cask - http://caskroom.io
