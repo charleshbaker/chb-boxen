aab53d6e0148ce5d687f0c8b3d07e33b4adbb5ac
Charles Baker <charleshbaker@gmail.com>
2015-08-21 21:34:12 -0400

    tmux use include for tmux instead of cask

--- modules/people/manifests/charleshbaker.pp
+++ modules/people/manifests/charleshbaker.pp
@@ -4,6 +4,7 @@ class people::charleshbaker {
   include brewcask # requires brewcask module in Puppetfile
   include chrome   # requires chrome module in Puppetfile
   include textmate::textmate2::beta
+  include tmux
   include zsh   # requires zsh module in Puppetfile
 
   ## Install these with Cask - http://caskroom.io
